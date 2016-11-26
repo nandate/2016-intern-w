c9c768d098214f2842bb752666b15e048eecf75c
nandate <o238049t@mail.kyutech.jp>
2016-11-27 04:42:26 +0900

    add blog post

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,29 @@
+class BlogsController < ApplicationController
+
+  def index
+    @blog=Blog.new
+    @blogs=Blog.all
+  end
+
+  def new
+    @blog=Blog.new
+  end
+
+  def show
+    @blog=Blog.find(params[:id])
+  end
+
+  def create
+    @blog=Blog.new(blog_params)
+    if @blog.sace
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def blog_params
+    params.require(:blog).permit(:body,:like)
+  end
+end
