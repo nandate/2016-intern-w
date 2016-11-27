295fb30cd4109243ba676da6f41b50eb49dcf0c5
nandate <o238049t@mail.kyutech.jp>
2016-11-27 13:40:06 +0900

    fix

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_intern-w-2016_session'
+Rails.application.config.session_store :cookie_store, key: '_intern_w2016_session'
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
   root 'blogs#index'
-
   get 'blogs/like'
   resources :blogs do
     resources :replies
   end
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
